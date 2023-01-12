61db867bcd39ed4d4c7cfbf31806a38659f10206
kenenwobodo <nwobodokenechukwu2@gmail.com>
2023-01-12 14:26:43 +0100

    Improved the application by implementing the copy quote feature, tweet quote and made the app responsve across devices.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Quotes-Generator
-This is an application that shows different kinds of quotes from wise men and women. Users can listen to each of these quotes by just a click too. More features would be added soon.
+
+This is an application that shows different kinds of quotes from wise men and women. Users can listen to each of these quotes by just a click too.
+
+The application includes the following features:
+
+1. Utilized fetch API to communicate with an external API. (https://api.quotable.io/random);
+2. Listen to each quote.
+3. Copy the quotes.
+4. Ability to tweet each quote on Twitter.
--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const quoteBtn = document.querySelector("button");
 const author = document.querySelector(".author .name");
 
 const soundIcon = document.querySelector(".sound");
+const copyBtn = document.querySelector(".copy");
+const twitterBtn = document.querySelector(".twitter");
 
 // Quotes function
 function GenerateQuotes() {
@@ -31,8 +33,26 @@ function SpeakText() {
   speechSynthesis.speak(utterance);
 }
 
+// Copy text function
+function CopyText() {
+  navigator.clipboard.writeText(quoteText.innerHTML);
+}
+
+// Twitter Redirect func
+function TwitterRedirect() {
+  const twitterURL = `https://twitter.com/intent/tweet?url=${quoteText.innerHTML}`;
+  // opens URL in a new windo
+  window.open(twitterURL, "_blank");
+}
+
 // Listen for event on the Quote Button
 quoteBtn.addEventListener("click", GenerateQuotes);
 
-// Listen for even on the Sound Icon
+// Listen for event on the Sound Icon
 soundIcon.addEventListener("click", SpeakText);
+
+// listen for event on the Copy Button
+copyBtn.addEventListener("click", CopyText);
+
+// listen for a tweet redirect
+twitterBtn.addEventListener("click", TwitterRedirect);
